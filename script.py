780d8305b585f6c1f1c526b435bb7a135d823de6
Jay Chow <s2600569@bioinfmsc5.ed.ac.uk>
2023-11-14 19:12:08 +0000

    queried for protein sequences with taxonid and pfam. Also gave user info about no. of sequences gathered

--- script.py
+++ script.py
@@ -7,25 +7,43 @@ import subprocess
 
 
 ##1. Gather user input for taxonomic group and protein family
-pfam = input("Enter Protein Family:\n")
-taxo = input("Enter Taxonomic Group:\n")
+print("-------------------------------------------")
+#pfam = input("Enter Protein Family:\n")
+pfam = "glucose-6-phosphatase"
+#pfam = "ABC transporters"
+
+#taxo = input("Enter Taxonomic Group:\n")
+taxo = "aves"
+#taxo = "mammals"
+
+print("-------------------------------------------")
 
 
 
 ##2. Gather desired protein sequences  (Include checks)
 #2a. Query for taxonID
+print("Gathering taxonID for " + taxo)
 esearchTaxoquery = "esearch -db taxonomy -spell -query \"" + taxo + "\" | efetch -format uid"
 esearchTaxoUID = subprocess.check_output(esearchTaxoquery, shell=True).decode("utf-8") #QUERY TAXONID
 print("TAXONID: " + str(esearchTaxoUID))
-#2b. Query for protein sequences
-esearchProtquery = "esearch -db protein -spell -query \"" + pfam + "\" | efetch -format "
+#2b. Query for protein sequences filtered with taxonID with :exp to get taxo subtree groups
+print("Gathering protein sequences of " + pfam + " in " + taxo + " txid:" +esearchTaxoUID + "...")
+esearchProtquery = "esearch -db protein -spell -query \"txid" + esearchTaxoUID + "[Organism:exp]" + " AND " + pfam + "[PROT]" + "\" | efetch -format fasta > seq.fasta"
+esearchProtfasta = os.system(esearchProtquery)
+print("Gathered")
+#2c. Give user more information about queried sequences (no. of seq)
+with open("seq.fasta") as infile:
+  infileread = infile.read()
+  seqcount = infileread.count(">")
+
+print("Number of sequences gathered: " + str(seqcount))
+#2d. Check if empty. If empty, rerequest pfam and taxo
 
 
 
-
-#einfo -db taxonomy -fields
+#######info -db taxonomy -fields
 #######need to use [Organism:exp] to get all associated taxID in subtree
-#########einfo -db protein -fields
+#######einfo -db protein -fields
 
 
 
