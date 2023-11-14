7c541202601580546bf4f239af4bdda47d5461c3
Jay Chow <s2600569@bioinfmsc5.ed.ac.uk>
2023-11-14 10:03:09 +0000

    First initial commit

---
+++ script.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+#Modules
+import os
+import sys
+
+
+##1. Gather user input for taxonomic group and protein family (Include checks)
+
+##2. Gather desired protein sequences 
+
+
+##3. BLAST/EMBOSS for sequence conservation
+
+
+##4. Scan for PROSITE motifs in sequences
+
+
+##5. Other EMBOSS analysis
