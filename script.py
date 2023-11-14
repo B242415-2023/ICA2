dd6270f7d85aa6feef67b92464481638bcef5bcb
Jay Chow <s2600569@bioinfmsc5.ed.ac.uk>
2023-11-14 17:35:12 +0000

    Gather user input and get taxonID

--- script.py
+++ script.py
@@ -1,19 +1,42 @@
 #!/usr/bin/python3
-
 #Modules
 import os
 import sys
+import subprocess
 
 
-##1. Gather user input for taxonomic group and protein family (Include checks)
 
-##2. Gather desired protein sequences 
+##1. Gather user input for taxonomic group and protein family
+pfam = input("Enter Protein Family:\n")
+taxo = input("Enter Taxonomic Group:\n")
 
 
-##3. BLAST/EMBOSS for sequence conservation
+
+##2. Gather desired protein sequences  (Include checks)
+#2a. Query for taxonID
+esearchTaxoquery = "esearch -db taxonomy -spell -query \"" + taxo + "\" | efetch -format uid"
+esearchTaxoUID = subprocess.check_output(esearchTaxoquery, shell=True).decode("utf-8") #QUERY TAXONID
+print("TAXONID: " + str(esearchTaxoUID))
+#2b. Query for protein sequences
+esearchProtquery = "esearch -db protein -spell -query \"" + pfam + "\" | efetch -format "
+
+
+
+
+#einfo -db taxonomy -fields
+#######need to use [Organism:exp] to get all associated taxID in subtree
+#########einfo -db protein -fields
+
+
+
+##3. CLUSTALO for alignment, EMBOSS for plotcon for sequence conservation
+
 
 
 ##4. Scan for PROSITE motifs in sequences
 
 
+
 ##5. Other EMBOSS analysis
+
+
