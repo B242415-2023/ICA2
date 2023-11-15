863d32fafce120852e91f926f7e957884d6d661a
Jay Chow <s2600569@bioinfmsc5.ed.ac.uk>
2023-11-15 07:52:55 +0000

    added error check of taxid and pfam

--- script.py
+++ script.py
@@ -4,6 +4,9 @@ import os
 import sys
 import subprocess
 
+#######################  OPTIONS  #######################
+max_number_of_sequences=1000
+
 
 
 ##1. Gather user input for taxonomic group and protein family
@@ -16,7 +19,6 @@ pfam = "glucose-6-phosphatase"
 #taxo = input("Enter Taxonomic Group:\n")
 taxo = "aves"
 #taxo = "mammals"
-
 print("-------------------------------------------")
 print("-------------------------------------------")
 
@@ -28,27 +30,40 @@ print("Gathering taxonID for " + taxo + "\n...")
 esearchTaxoquery = "esearch -db taxonomy -spell -query \"" + taxo + "\" | efetch -format uid"
 esearchTaxoUID = subprocess.check_output(esearchTaxoquery, shell=True).decode("utf-8").rstrip() #QUERY TAXONID
 print("TAXONID: " + str(esearchTaxoUID) + "\n...\nGathered")
+
+if (esearchTaxoUID == ""): #error check
+  print("Invalid taxonomic group\nExiting program...")
+  exit()
+  
 print("-------------------------------------------")
+
 #2b. Query for protein sequences filtered with taxonID with :exp to get taxo subtree groups
 print("Gathering protein sequences of " + pfam + " in " + taxo + " txid:" +esearchTaxoUID + "\n...")
 esearchProtquery = "esearch -db protein -spell -query \"txid" + esearchTaxoUID + "[Organism:exp]" + " AND " + pfam + "[PROT]" + "\" | efetch -format fasta > seq.fasta"
 esearchProtfasta = os.system(esearchProtquery)
 print("Gathered")
+
 #2c. Give user more information about queried sequences (no. of seq)
 with open("seq.fasta") as infile:
   infileread = infile.read()
   seqcount = infileread.count(">")
 
 print("Number of sequences gathered: " + str(seqcount))
+
+if (seqcount == 0): #error check
+  print("Invalid protein family\nExiting program...")
+  exit()
+elif (seqcount > max_number_of_sequences):
+  print("Exceeds max number of sequences, " + str(max_number_of_sequences) + "\nExiting program...")
+  exit()
+  
 print("-------------------------------------------")
-#2d. Check if empty. If empty, rerequest pfam and taxo (may have to switch order so its first)
 
 
 
 #######info -db taxonomy -fields
 #######need to use [Organism:exp] to get all associated taxID in subtree
 #######einfo -db protein -fields
-
 
 
 ##3. CLUSTALO for alignment, EMBOSS for plotcon for sequence conservation
