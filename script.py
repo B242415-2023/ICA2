f59ace1b478b2fb58156e23a7b84a6079c05e71c
Jay Chow <s2600569@bioinfmsc5.ed.ac.uk>
2023-11-17 11:09:14 +0000

    patmatmotif prosite motif finder done

--- script.py
+++ script.py
@@ -8,20 +8,21 @@ import subprocess
 max_number_of_sequences=1000
 availthreads = subprocess.check_output("nproc", shell=True).decode("utf-8").rstrip()
 
-
+#Folders
+os.system("mkdir motifs")
 
 ##1. Gather user input for taxonomic group and protein family
 print("-------------------------------------------")
 print("-------------------------------------------")
 #pfam = input("Enter Protein Family:\n")
 pfam = "glucose-6-phosphatase"
-#pfam = "ABC transporters"
+#pfam = "ABC transporter"
 
 #taxo = input("Enter Taxonomic Group:\n")
 taxo = "aves"
 #taxo = "mammals"
-
-
+#pfam = "kinase"
+#taxo = "rodents"
 #pfam = "adenyl cyclases"
 #taxo = "vertebrates"
 
@@ -46,8 +47,10 @@ print("-------------------------------------------")
 
 #2b. Query for protein sequences filtered with taxonID with :exp to get taxo subtree groups
 print("Gathering protein sequences of " + pfam + " in " + taxo + " txid:" +esearchTaxoUID + "\n...")
+
 esearchProtquery = "esearch -db protein -spell -query \"txid" + esearchTaxoUID + "[Organism:exp]" + " AND " + pfam + "[PROT]" + "\" | efetch -format fasta > seq.fasta"
 esearchProtfasta = os.system(esearchProtquery)
+
 print("Gathered")
 
 #2c. Give user more information about queried sequences (no. of seq)
@@ -76,30 +79,57 @@ print("-------------------------------------------")
 ##3. CLUSTALO for alignment, EMBOSS for plotcon for sequence conservation plot
 #3a. ClustalO
 print("Aligning sequences via ClustalO with: " + availthreads + " threads\n...")
+
 os.system("clustalo -i seq.fasta -o aligned.fasta --force --threads=" + str(availthreads))
+
 print("Aligned")
 print("-------------------------------------------")
 
 #3b.  plotcon
 print("Plotting convservation of seuqnece alignment\n...")
+
 os.system("plotcon -sequences aligned.fasta -winsize=4 -graph png -sprotein1")
+
 print("Plotted")
 print("-------------------------------------------")
 
 
 
 ##4. Scan for PROSITE motifs in sequences patmatmotifs
+print("Scanning sequences for motifs\n...")
 #4a. Separate sequences in seq.fasta
-
 with open("seq.fasta") as infile:
   allseq = infile.read().rstrip().split(">")
+  allseq1 = [">" + seqelement for seqelement in allseq]
+
+#4bi. Create list of headers
+seqheaders = []
+for myseq in allseq1:
+  headerend = myseq.find("\n")
+  header = myseq[:headerend]
+  seqheaders.append(header)
   
+#4bii. Create dictionary of header:fasta
+seqdict = {}
+i=0
+for header in seqheaders:
+  seqdict[header] = allseq1[i]
+  i=i+1
+  
+#4c. Run patmatmotifs on each seq in allseq1
+for header in seqheaders:
+  with open("temp.fasta", "w") as infile:
+    infile.write(seqdict.get(header))
+  headerfileformat = header.replace(">", "").replace(" ", "_").replace("[", "").replace("]", "").replace(",", "").replace(":", "")
+  os.system("patmatmotifs -full -sequence temp.fasta -outfile ./motifs/" + headerfileformat + ".patmatmotifs")
+  
+print("Done. Results in ./motifs/")
 
 #patmatmotifs -sequence seq.fasta -outfile motif.txt
 
 ##5. Other EMBOSS analysis
 
-
+########remove plurals
 
 #####if statement for different esearch filters, remove 'associated'
-
+##########switch error checks to try, except       to catch all errors and outcomes
